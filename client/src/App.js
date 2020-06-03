63d55395006ba4273a9422e03ece095e59d7ea5f
EgorOnufreychuk <58828023+EgorOnufreychuk@users.noreply.github.com>
2020-06-03 18:16:29 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header/';
 import Side_bar from './components/Header/side_bar';
 
 
