c2cc2021263fd8e0c4f6c9b9bc7fcf2584547667
EgorOnufreychuk <onufreuchuk.egor@rl.odessa.ua>
2020-06-06 18:50:48 +0300

    Link integration

--- client/src/components/Header/side_bar.js
+++ client/src/components/Header/side_bar.js
@@ -1,32 +1,73 @@
 import React, {Component} from 'react';
 import {withStyles} from '@material-ui/core';
 import Drawer from '@material-ui/core/Drawer';
-import MailIcon from '@material-ui/icons/Mail';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 import ListItem from '@material-ui/core/ListItem';
+import CodeIcon from '@material-ui/icons/Code';
+import TocIcon from '@material-ui/icons/Toc';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
 
 const styles = {
     search: {
 
     }
 }
-class Side_bar extends Component {
+class Sidebar extends Component {
     render() {
-        const { classes } = this.props;
-        console.log(this.props)
+        // const { classes } = this.props; (not used)
+        // console.log(this.props)
       return (
-        <Drawer anchor={"left"} open={this.props.isOpen} onClose={this.props.closeDrawer}>
-            <div className="sidebar-navigation">
-                <ListItem button>
-                <ListItemIcon>{<MailIcon/>}</ListItemIcon>
-                <ListItemText primary={'Home'} />
-                </ListItem>
-            </div>
-        </Drawer>
+        <Router>
+          <Drawer anchor={"left"} open={this.props.isOpen} onClose={this.props.closeDrawer}>
+              <nav>
+                <div className="sidebar-navigation">
+                    <ListItem >
+                      <Link to="/info">
+                        <ListItemIcon>{<TocIcon/>}</ListItemIcon>
+                        Information
+                      </Link>
+                    </ListItem>
+                    <ListItem >
+                      <Link to="/bugs">
+                        <ListItemIcon>{<CodeIcon/>}</ListItemIcon>
+                        Bugs info
+                      </Link>
+                    </ListItem>
+                </div>
+              </nav>
+          </Drawer>
+          <Switch>
+            <Route path="/:id" children={<Child />} />
+          </Switch>
+        </Router>
       );
     }
   }
-  
-export default withStyles(styles)(Side_bar);
+function Child() {
+  let { id } = useParams();
+  if (id === 'info') {
+    console.log('Information button was clicked !')
+    return (
+      <div>
+        <h3>In development!</h3>
+      </div>
+  )} else if (id === 'bugs') {
+    console.log("Bugs info button was clicked !")
+    return (
+      <div>
+        <h1>
+          Warning: findDOMNode is deprecated in StrictMode !
+        </h1>
+      </div>
+
+    )
+  }
+}   
+export default withStyles(styles)(Sidebar);
   
