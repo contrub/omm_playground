a82fc8f370441f1933827a95b5ea75ff806b0110
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2020-06-17 19:06:15 +0300

    Function 'MonumentById' created

---
+++ client/src/pages/index.js
@@ -0,0 +1,28 @@
+﻿import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
+const useStyles = makeStyles((theme) => ({
+    page: {
+        margin: '3%',
+    },
+    pageName: {
+        textAlign: "center",
+    },
+    image: {
+        float: 'right',
+        margin: '0 0 5px 5px' 
+    }
+}));
+export default function MonumentById(props) {
+    const classes = useStyles();
+    return (
+        <div className={classes.page}>
+            <h1 className={classes.pageName}>{props.title}</h1>
+            <p>
+                <img alt='' width="40%" className={classes.image} src={props.src}/>
+                {props.description}
+                </p>
+            <Link to="/monuments">Back</Link>
+        </div>
+    )
+}
