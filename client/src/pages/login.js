41f34fb231c0c94602c82985d74554decaa3c6d4
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2020-09-26 11:45:53 +0300

    validation info changed

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,5 +1,5 @@
 ﻿import React from "react";
-import { Link }  from "react-router-dom";
+import {Link} from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
@@ -10,8 +10,6 @@ import Container from "@material-ui/core/Container";
 import Avatar from "@material-ui/core/Avatar";
 import Checkbox from "@material-ui/core/Checkbox";
 import withStyles from "@material-ui/core/styles/withStyles";
-import isEmail from "validator/es/lib/isEmail";
-import isEmpty from "validator/es/lib/isEmpty";
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
 
@@ -42,7 +40,7 @@ const styles = theme => ({
   }
 });
 
-class Login extends React.Component {
+class SignIn extends React.Component {
 
   constructor(props){
     super(props);
@@ -52,100 +50,24 @@ class Login extends React.Component {
         email: '',
         password: ''
       },
-      errors: {},
+      db: [
+        {login: 'admin@gmail.com', password: 'adminADMIN1234!'},
+        {login: 'user@gmail.com', password: 'userUSER1234!'}
+      ],
       isValid: false
     }
   }
 
-  handleFieldValidation = (name) => {
-    let inputs = this.state.inputs;
-    let errors = this.state.errors;
-    console.log(inputs)
-    console.log(errors)
-    if (name === "email") {
-      if (inputs["email"] !== undefined) {
-        if (isEmpty(inputs["email"])){
-          errors["email"] = "Cannot be empty";
-          this.setState({isValid: false})
-        } else {
-          if (!isEmail(inputs["email"])) {
-            errors["email"] = "Email is not valid";
-            this.setState({isValid: false})
-          } else {
-            errors["email"] = ""
-            this.setState({isValid: true})
-          }
-        }
-      } else {
-        errors["email"] = "Cannot be empty";
-        this.setState({isValid: false})
-      }
-    } else if (name === "password") {
-      if (inputs["password"] !== undefined) {
-        if (isEmpty(inputs["password"])) {
-          errors["password"] = "Cannot be empty";
-          this.setState({isValid: false})
-        } else {
-
-          const mediumRegex = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
-          const strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#%&])(?=.{8,})");
-
-          if (!mediumRegex.test(inputs["password"])) {
-            errors["password"] = "low password";
-            this.setState({isValid: false})
-          } else if (!strongRegex.test(inputs["password"])) {
-            errors["password"] = "medium password";
-            this.setState({isValid: false})
-          } else {
-            errors["password"] = ""
-            if (inputs["password"] === inputs["passwordCopy"]) {
-              errors["passwordCopy"] = ""
-            }
-            this.setState({isValid: true})
-          }
-        }
-      } else {
-        errors["password"] = "Cannot be empty";
-        this.setState({isValid: false})
-      }
-    } else {
-      if (inputs["passwordCopy"] !== undefined) {
-        if (isEmpty(inputs["passwordCopy"])) {
-          errors["passwordCopy"] = "Cannot be empty";
-          this.setState({isValid: false})
-        } else {
-          if (errors.password !== "") {
-            errors["passwordCopy"] = "Password wasn't validated"
-            this.setState({isValid: false})
-          } else if (inputs.password !== inputs.passwordCopy) {
-            errors["passwordCopy"] = "Repeat password!";
-            this.setState({isValid: false})
-          } else {
-            errors["passwordCopy"] = ""
-            this.setState({isValid: true})
-          }
-        }
-      } else {
-        errors["passwordCopy"] = "Cannot be empty";
-        this.setState({isValid: false})
-      }
-    }
-    if (inputs["email"] === '' || inputs["password"] === '' || inputs["passwordCopy"] === '') {
-      this.setState({isValid: false})
-    }
-    this.setState({errors: errors});
-  }
-
   contactSubmit = (e) => {
     e.preventDefault();
-
-    if (this.state.isValid) {
-      alert("Form submitted");
+    let inputs = this.state.inputs;
+    let result = this.state.db.find(user => user.password === inputs.password)
+    if (result === undefined) {
+      alert('User undefined')
     } else {
-      this.handleFieldValidation('email')
-      this.handleFieldValidation('password')
-      this.handleFieldValidation('passwordCopy')
-      alert("Validation error")
+      console.log(result)
+      alert('Successful login')
+      window.location.href = '/monuments' // заменить на react component
     }
   }
 
@@ -153,22 +75,20 @@ class Login extends React.Component {
     let inputs = this.state.inputs;
     inputs[input] = e.target.value;
     this.setState({input: inputs[input]});
-    this.handleFieldValidation(input)
   }
 
   showPassword = () => {
     if (document.getElementById('password').type === 'password') {
       document.getElementById('password').type = 'text'
-      document.getElementById('passwordCopy').type = 'text'
     } else {
       document.getElementById('password').type = 'password'
-      document.getElementById('passwordCopy').type = 'password'
     }
   }
-// 1. Вилидация input с value type "undefined" только при помощи лог.условия (inputs['email'] !== undefined) ?
+
   render() {
 
     const { classes } = this.props;
+    console.log(this.state)
 
     return (
         <Container component="main" maxWidth="xs" onSubmit= {this.contactSubmit.bind(this)}>
@@ -192,7 +112,6 @@ class Login extends React.Component {
                   autoComplete="email"
                   autoFocus
                 />
-                <div className={classes.errors}>{this.state.errors["email"]}</div>
                 <TextField
                   onChange={this.handleChange.bind(this, "password")}
                   value={this.state.inputs["password"]}
@@ -206,21 +125,6 @@ class Login extends React.Component {
                   id="password"
                   autoComplete="current-password"
                 />
-                <div className={classes.errors}>{this.state.errors["password"]}</div>
-                <TextField
-                  onChange={this.handleChange.bind(this, "passwordCopy")}
-                  value={this.state.inputs["passwordCopy"]}
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="passwordCopy"
-                  label="Repeat Password"
-                  type="password"
-                  id="passwordCopy"
-                  autoComplete="current-password"
-                />
-                <div className={classes.errors}>{this.state.errors["passwordCopy"]}</div>
                 <FormControlLabel
                   control={<Checkbox value="remember" color="primary" />}
                   label="Remember me"
@@ -260,5 +164,5 @@ class Login extends React.Component {
 }
 
 //  Где будут храниться логины и пароли пользователей + в каком виде ? (https://habr.com/ru/company/acribia/blog/413157/)
-export default withStyles(styles, {withTheme: true})(Login)
+export default withStyles(styles, {withTheme: true})(SignIn)
 
