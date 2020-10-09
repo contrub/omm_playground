bb8df628a3f819fc216d069e2fb2cdca2d8a5c41
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2020-10-09 23:27:22 +0300

    user schema created'

---
+++ api/models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  userType: {
+    type: String,
+    enum : ['user','admin'],
+    default: 'user'
+  },
+}, {
+  versionKey: false
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
