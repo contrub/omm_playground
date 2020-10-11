5e22751de6c4e4386f8bc393694f71c95e657550
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2020-10-11 17:09:09 +0300

    getUser fixed

--- client/src/services/UserSevice.js
+++ client/src/services/UserSevice.js
@@ -4,13 +4,16 @@ export default {
   getUsers() {
     return api.get("/users");
   },
+  getUser(email) {
+    return api.get(`/users/${email}`)
+  },
   createUser(params) {
     return api.create("/users", params);
   },
-  deleteUser(id) {
-    return api.remove(`/users/${id}`);
+  deleteUser(email) {
+    return api.remove(`/users/${email}`);
   },
   updateUser(params) {
-    return api.update(`/users/${params._id}`, params)
+    return api.update(`/users/${params.email}`, params)
   }
 };
