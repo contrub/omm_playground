00ac1c6604e51719f85d557ed4c9be765d024e38
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2020-10-30 00:21:44 +0200

    methods modifications

--- client/src/services/UserSevice.js
+++ client/src/services/UserSevice.js
@@ -1,19 +1,19 @@
 import api from "./api";
 
 export default {
+  createUser(params) {
+    return api.create("/users", params);
+  },
   getUsers() {
-    return api.get("/users");
+    return api.get("/users")
   },
   getUser(email) {
     return api.get(`/users/${email}`)
   },
-  createUser(params) {
-    return api.create("/users", params);
+  updateUser(params) {
+    return api.update(`/users/${params.email}`, params)
   },
   deleteUser(email) {
     return api.remove(`/users/${email}`);
-  },
-  updateUser(params) {
-    return api.update(`/users/${params.email}`, params)
   }
 };
