f778970f66d73841868a6b84d352825c9314baa3
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2020-12-28 23:54:45 +0200

    jwt verification, when GET request

--- client/src/pages/monuments.js
+++ client/src/pages/monuments.js
@@ -1,46 +1,65 @@
 import React from 'react';
-import MonumentService from '../services/MonumentService'
-import './monuments.css'
-import {Link} from "react-router-dom";
+import MonumentService from '../services/MonumentService';
+import '../styles/monuments.css';
 
 class Monuments extends React.Component {
   state = {
     monuments: []
   }
 
-  componentDidMount = async() => {
-    console.log('Fetching monuments...')
-    await MonumentService.fetchMonuments()
-      .then(result => {
-        this.setState({
-          monuments: result
-        })
-      })
-    if (this.state.monuments.length === 0) {
-      document.getElementById('monuments-page').innerHTML = `<div className='error'>There are no monuments :(</div>`
+  getAccessToken = () => {
+    let cookies = document.cookie;
+    let cookiesArray = cookies.split(';')
+    for (let i = 0; i < cookiesArray.length; i++) {
+      let name = cookiesArray[i].split('=')[0];
+      let value = cookiesArray[i].split('=')[1];
+      if (name === 'accessToken') {
+        return value
+      }
     }
+    return null
+  }
+
+  componentDidMount = async() => {
+
+    let accessToken = this.getAccessToken()
+    console.log('accessToken : ' + accessToken)
+
+    // await MonumentService.fetchMonuments({
+    //   Authorization: accessToken
+    // })
+    //   .then(result => {
+    //     this.setState({
+    //       monuments: result
+    //     })
+    //   })
+    // if (this.monuments.status) {
+    //   document.getElementById('monuments-page').innerHTML = 'Unauthorized user!'
+    // } else {
+    //   if (this.state.monuments.length === 0) {
+    //     document.getElementById('monuments-page').innerHTML = 'Successful authorize!'
+    //   }
+    // }
   }
 
   render() {
 
-    const {monuments} = this.state
-    const result = monuments.map((entry) => {
-      return (
-        <div className='holder' key={entry._id}>
-          <img alt='monument' style={{maxWidth: '100%', maxHeight: '100%'}} src="https://ua.igotoworld.com/frontend/webcontent/websites/1/images/gallery/20636_800x600_odessa.jpg"/>
-          <Link to={`monuments/${entry._id}`}>
-            <div className='block'>
-              {entry.name}
-            </div>
-          </Link>
-        </div>
-      )
-    })
+    // const result = monuments.map((entry) => {
+    //   return (
+    //     <div className='holder' key={entry._id}>
+    //       <img alt='monument' style={{maxWidth: '100%', maxHeight: '100%'}} src="https://ua.igotoworld.com/frontend/webcontent/websites/1/images/gallery/20636_800x600_odessa.jpg"/>
+    //       <Link to={`monuments/${entry._id}`}>
+    //         <div className='block'>
+    //           {entry.name}
+    //         </div>
+    //       </Link>
+    //     </div>
+    //   )
+    // })
 
     return (
       <div id='monuments-page'>
         <div className='monuments-list'>
-          {result}
         </div>
       </div>
     )
