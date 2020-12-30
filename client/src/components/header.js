4d9e386d386dad90267258273e4cf8bc2ec049e6
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2020-12-30 21:20:58 +0200

    login button deleted

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -2,13 +2,13 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import InputBase from '@material-ui/core/InputBase';
 import SearchIcon from '@material-ui/icons/Search';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logout from '../services/UserSevice'
 
 const styles =  {
   root: {
