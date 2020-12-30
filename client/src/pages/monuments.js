5520751c65b29f3d53ac412e4135bdd6171a8d4c
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2020-12-30 21:21:54 +0200

    modified

--- client/src/pages/monuments.js
+++ client/src/pages/monuments.js
@@ -2,6 +2,9 @@ import React from 'react';
 import MonumentService from '../services/MonumentService';
 import '../styles/monuments.css';
 
+import potemkins_stairs_1 from '../images/The Potemkin Stairs/potemkins_stairs_1.jpg'
+import potemkins_stairs_2 from '../images/The Potemkin Stairs/potemkins_stairs_2.jpeg'
+
 class Monuments extends React.Component {
   state = {
     monuments: []
@@ -17,29 +20,17 @@ class Monuments extends React.Component {
         return value
       }
     }
-    return null
+    return undefined
   }
 
   componentDidMount = async() => {
 
-    let accessToken = this.getAccessToken()
-    console.log('accessToken : ' + accessToken)
-
-    // await MonumentService.fetchMonuments({
-    //   Authorization: accessToken
-    // })
-    //   .then(result => {
-    //     this.setState({
-    //       monuments: result
-    //     })
-    //   })
-    // if (this.monuments.status) {
-    //   document.getElementById('monuments-page').innerHTML = 'Unauthorized user!'
-    // } else {
-    //   if (this.state.monuments.length === 0) {
-    //     document.getElementById('monuments-page').innerHTML = 'Successful authorize!'
-    //   }
-    // }
+    await MonumentService.fetchMonuments()
+      .then((res) => {
+        this.setState({
+          monuments: res
+        })
+      })
   }
 
   render() {
@@ -59,7 +50,41 @@ class Monuments extends React.Component {
 
     return (
       <div id='monuments-page'>
-        <div className='monuments-list'>
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"/>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"/>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"/>
+        <div className="container">
+          <h3 className="h3">shopping Demo-1 </h3>
+          <div className="row">
+            <div className="col-md-3 col-sm-6">
+              <div className="product-grid">
+                <div className="product-image">
+                  <a href="/mon1">
+                    <img className="pic-1" src={potemkins_stairs_1} alt='img1'/>
+                      <img className="pic-2" src={potemkins_stairs_2} alt='img2'/>
+                  </a>
+                </div>
+                <ul className="rating">
+                  <li className="fa fa-star"></li>
+                  <li className="fa fa-star"></li>
+                  <li className="fa fa-star"></li>
+                  <li className="fa fa-star"></li>
+                  <li className="fa fa-star disable"></li>
+                </ul>
+                <div className="product-content">
+                  <h3 className="title">
+                    <a href="/mon2">
+                      The Potemkin Stairs
+                    </a>
+                  </h3>
+                  <div className="year">
+                    Год ония: 1825
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
     )
