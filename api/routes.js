71b58672cb6a172d6ec3592a3aaf8f58de8521b7
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2021-01-24 03:07:20 +0200

    modified

--- api/routes.js
+++ api/routes.js
@@ -4,18 +4,32 @@ const verifyToken = require('./utils/verifyToken')
 
 const Monuments = require('./routes/Monuments')
 const Users = require('./routes/Users')
+const UserService = require('./helpers/UserService')
+const AuthController = require('./Controllers/AuthController')
 
-routes.get('/monuments', verifyToken.AccessToken, Monuments.getMonuments)
-routes.get('/monuments/:id', verifyToken.AccessToken, Monuments.getMonument)
-routes.put('/monuments/:id', Monuments.updateMonument)
-routes.delete('/monuments/:id', Monuments.deleteMonument)
-routes.delete('/monuments/all', Monuments.clearMonumentsDB)
+// Monuments
 
-routes.get('/users', Users.getUsers)
-routes.get('/users/:id', Users.getUser)
-routes.post('/users', Users.createUser)
-routes.put('/users/:id', Users.updateUser)
-routes.delete('/users/:id', Users.deleteUser)
-routes.delete('/users/all', Users.clearUserDB)
+routes.get('get/monuments', verifyToken.AccessToken, Monuments.getMonuments)
+routes.get('get/monuments/:id', verifyToken.AccessToken, Monuments.getMonument)
+
+routes.post('post/monuments', Monuments.createMonument)
+
+routes.put('put/monuments/:id', Monuments.updateMonument)
+
+routes.delete('delete/monuments/:id', Monuments.deleteMonument)
+routes.delete('delete/monuments/db/all', Monuments.clearMonumentsDB)
+
+// Users
+
+routes.get('get/users', Users.getUsers)
+routes.get('get/users/:email', Users.getUser)
+
+routes.post('/login', UserService.isUserDataExist, AuthController.getTokens)
+routes.post('post/users', UserService.isUserExist, AuthController.getTokens, Users.createUser)
+
+routes.put('put/users/:email', Users.updateUser)
+
+routes.delete('delete/users/:email', Users.deleteUser)
+routes.delete('delete/users/db/all', Users.clearUserDB)
 
 module.exports = routes
