37fb049abedbbcba30678466312223ade90d5315
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2021-01-27 17:14:01 +0200

    wrong ways fixed

--- client/src/services/UserService.js
+++ client/src/services/UserService.js
@@ -5,18 +5,18 @@ export default {
     return api.create(`/login`, params)
   },
   createUser(params) {
-    return api.create("post/users", params);
+    return api.create("/post/users", params);
   },
   getUsers() {
-    return api.get("get/users")
+    return api.get("/get/users")
   },
   getUser(params) {
-    return api.get(`get/users/${params.email}`, params)
+    return api.get(`/get/users/${params.email}`, params)
   },
   updateUser(params) {
-    return api.update(`put/users/${params.email}`, params)
+    return api.update(`/put/users/${params.email}`, params)
   },
   deleteUser(email) {
-    return api.remove(`delete/users/${email}`);
+    return api.remove(`/delete/users/${email}`);
   }
 };
