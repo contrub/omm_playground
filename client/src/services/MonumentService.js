5620f515d581a299fbd71aafc2bb6846e8954c36
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2021-01-27 17:11:28 +0200

    wrong ways fixed

--- client/src/services/MonumentService.js
+++ client/src/services/MonumentService.js
@@ -2,15 +2,15 @@ import api from "./api";
 
 export default {
   fetchMonuments(params, tokens) {
-    return api.get("get/monuments", params, tokens);
+    return api.get("/get/monuments", params, tokens);
   },
   createMonument(params) {
-    return api.create("post/monuments", params);
+    return api.create("/post/monuments", params);
   },
   deleteMonument(id) {
-    return api.remove(`delete/monuments/${id}`);
+    return api.remove(`/delete/monuments/${id}`);
   },
   updateMonument(params) {
-    return api.update(`put/monuments/${params._id}`, params)
+    return api.update(`/put/monuments/${params._id}`, params)
   }
 };
