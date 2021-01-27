b871e7590c8787c7e72e4847400ed571b4affb73
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2021-01-27 14:41:30 +0200

    verifyToken on every route added

--- api/routes.js
+++ api/routes.js
@@ -1,35 +1,34 @@
 const routes = require('express').Router()
 
-const verifyToken = require('./utils/verifyToken')
-
 const Monuments = require('./routes/Monuments')
 const Users = require('./routes/Users')
 const UserService = require('./helpers/UserService')
 const AuthController = require('./Controllers/AuthController')
+const jwt = require('./utils/jwt')
 
 // Monuments
 
-routes.get('get/monuments', verifyToken.AccessToken, Monuments.getMonuments)
-routes.get('get/monuments/:id', verifyToken.AccessToken, Monuments.getMonument)
+// routes.get('/get/monuments', verifyToken.AccessToken, Monuments.getMonuments)
+// routes.get('/get/monuments/:id', verifyToken.AccessToken, Monuments.getMonument)
 
-routes.post('post/monuments', Monuments.createMonument)
+routes.post('/post/monuments', jwt.verifyAccessToken, Monuments.createMonument)
 
-routes.put('put/monuments/:id', Monuments.updateMonument)
+routes.put('/put/monuments/:id', jwt.verifyAccessToken, Monuments.updateMonument)
 
-routes.delete('delete/monuments/:id', Monuments.deleteMonument)
-routes.delete('delete/monuments/db/all', Monuments.clearMonumentsDB)
+routes.delete('/delete/monuments/:id', jwt.verifyAccessToken, Monuments.deleteMonument)
+routes.delete('/delete/monuments/db/all', jwt.verifyAccessToken, Monuments.clearMonumentsDB)
 
 // Users
 
-routes.get('get/users', Users.getUsers)
-routes.get('get/users/:email', Users.getUser)
+routes.get('/get/users', jwt.verifyAccessToken, Users.getUsers)
+routes.get('/get/users/:email', jwt.verifyAccessToken, Users.getUser)
 
-routes.post('/login', UserService.isUserDataExist, AuthController.getTokens)
-routes.post('post/users', UserService.isUserExist, AuthController.getTokens, Users.createUser)
+// routes.post('/login', UserService.isUserDataExist, AuthController.getTokens)
+routes.post('/post/users', jwt.verifyAccessToken, UserService.isUserExist, AuthController.getTokens, Users.createUser)
 
-routes.put('put/users/:email', Users.updateUser)
+routes.put('/put/users/:email', jwt.verifyAccessToken, Users.updateUser)
 
-routes.delete('delete/users/:email', Users.deleteUser)
-routes.delete('delete/users/db/all', Users.clearUserDB)
+routes.delete('/delete/users/:email', jwt.verifyAccessToken, Users.deleteUser)
+routes.delete('/delete/users/db/all', jwt.verifyAccessToken, Users.clearUserDB)
 
 module.exports = routes
