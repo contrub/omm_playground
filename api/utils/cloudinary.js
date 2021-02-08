5d4090666503ad25d10d686fa9f64031b16c30e8
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2021-02-08 20:03:01 +0200

    uploadImage to cloudinary intagration

---
+++ api/utils/cloudinary.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const cloudinary = require('cloudinary').v2
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+uploadImage = async(req, res, next) => {
+  try {
+    const fileStr = req.body.base64;
+    if (fileStr === undefined) {
+      next()
+    }
+    await cloudinary.uploader.upload(fileStr, {
+      folder: 'monuments'
+    })
+      .then((uploadResponse) => {
+        const url = uploadResponse.secure_url
+        req.body.imageURL = url
+        delete req.body.base64
+        next()
+      })
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ err: 'Something went wrong' });
+  }
+}
+
+module.exports = {
+
+  uploadImage: uploadImage
+
+}
