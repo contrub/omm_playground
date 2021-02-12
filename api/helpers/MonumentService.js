ae5c5ce68890dd309cad2bef337e295b88a8f7a3
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2021-02-12 15:51:15 +0200

    validateData integration + fix bugs

--- api/helpers/MonumentService.js
+++ api/helpers/MonumentService.js
@@ -1,16 +1,18 @@
 const Monument = require('../models/Monument')
+const isEmpty = require('validator/lib/isEmpty')
 
 const isMonumentExist = (req, res, next) => {
   const name = req.body.name
-  if (name === undefined) {
-    res.status(404).send('Monument name undefined')
+
+  if (name === undefined || isEmpty(name)) {
+    res.status(404).json({message: 'Monument name undefined'})
   }
 
   Monument
     .find({name: req.body.name})
     .then((item) => {
       if (item.length) {
-        res.status(409).send('Monument already exist!')
+        res.status(400).json({message: 'Monument already exist!'}) // возможно, изменить на дургой статус код
       }
       else {
         next()
@@ -19,11 +21,28 @@ const isMonumentExist = (req, res, next) => {
     .catch(err => {
       res.sendStatus(500)
       console.log(err)
-    });
+    })
+}
+
+validateData = (req, res, next) => {
+
+  const name = req.body.name
+
+  if (name === undefined) {
+
+    res.status(404).json({message: "Monument name undefined"})
+
+  } else {
+
+    next()
+
+  }
+
 }
 
 module.exports = {
 
-  isMonumentExist: isMonumentExist
+  isMonumentExist: isMonumentExist,
+  validateData: validateData
 
 }
