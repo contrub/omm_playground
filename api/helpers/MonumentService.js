8441fb0298d4f89e0eaf588d8d1a43f49556a764
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2021-02-21 15:18:41 +0200

    modified

--- api/helpers/MonumentService.js
+++ api/helpers/MonumentService.js
@@ -4,19 +4,13 @@ const isEmpty = require('validator/lib/isEmpty')
 const isMonumentExist = (req, res, next) => {
   const name = req.body.name
 
-  if (name === undefined || isEmpty(name)) {
-    res.status(404).json({message: 'Monument name undefined'})
-  }
+  if (name === undefined || isEmpty(name)) res.status(404).json({message: 'Monument name undefined'})
 
   Monument
     .find({name: req.body.name})
     .then((item) => {
-      if (item.length) {
-        res.status(400).json({message: 'Monument already exist!'}) // возможно, изменить на дургой статус код
-      }
-      else {
-        next()
-      }
+      if (item.length) res.status(409).json({message: 'Monument already exist!'})
+      else next()
     })
     .catch(err => {
       res.sendStatus(500)
@@ -25,18 +19,10 @@ const isMonumentExist = (req, res, next) => {
 }
 
 validateData = (req, res, next) => {
-
   const name = req.body.name
 
-  if (name === undefined) {
-
-    res.status(404).json({message: "Monument name undefined"})
-
-  } else {
-
-    next()
-
-  }
+  if (name === undefined || isEmpty(name)) res.status(404).json({message: "Monument name undefined"})
+  else next()
 
 }
 
