21106af94a350c9e4f1306918c870d1b58aa1564
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2021-02-21 15:17:30 +0200

    rethought status code

--- api/helpers/UserService.js
+++ api/helpers/UserService.js
@@ -1,32 +1,22 @@
 const User = require('../models/User')
 const isEmail = require('validator/lib/isEmail')
 const isEmpty = require('validator/lib/isEmpty')
-const superadmins = require('../Controllers/superadmin.json')
 const bcrypt = require('bcrypt')
 
 const isUserExist = (req, res, next) => {
   const email = req.body.email
-  if (email === undefined || isEmpty(email)) {
-
-    res.status(404).json({message: 'Email undefined'})
-
-  } else {
-
+  if (email === undefined || isEmpty(email)) res.status(404).json({message: 'Email undefined'})
+  else {
     User
       .find({email: email})
       .then((item) => {
-        if (item.length !== 0) res.status(404).json({message: 'User already exist'})
-        else if (superadmins.some(admin => admin.email === email)) {
-          res.status(404).json({message: 'User already exist'})
-        } else {
-          next()
-        }
+        if (item.length) res.status(409).json({message: 'User already exist'})
+        else next()
       })
       .catch(err => {
         res.sendStatus(500)
         console.log(err)
-      });
-
+      })
   }
 }
 
@@ -34,75 +24,37 @@ const isUserDataExist = (req, res, next) => {
   const email = req.body.email
   const password = req.body.password
 
-  if (email === undefined || password === undefined) {
+  if (email === undefined || isEmpty(email)) res.status(404).json({message: 'Email undefined'})
 
-    res.status(404).json({message: 'Login payload undefined'})
+  else if (password === undefined || isEmpty(password)) res.status(404).json({message: 'Password undefined'})
 
-  } else {
+  else {
     User
       .find({email: email})
       .then((item) => {
-
         if (item.length) {
-
           let savedPassword = item[0].password
           const matches = bcrypt.compareSync(password, savedPassword)
-
           if (matches) next()
-
-          else {
-
-            res.status(423).json({message: 'Password error'})
-
-          }
-
-        } else if (superadmins.some(admin => admin.email === email)) {
-
-          const savedPassword = superadmins.find(superadmin => superadmin.email === email).password
-          const matches = bcrypt.compareSync(password, savedPassword)
-
-          if (matches) next()
-
-          else {
-
-            res.status(423).json({message: 'Password error'})
-
-          }
-
-        } else {
-
-          res.status(404).json({message: 'User not found'})
-
+          else res.status(404).json({message: 'Password error'})
         }
+        else res.status(404).json({message: 'User not found'})
       })
       .catch(err => {
-
         res.sendStatus(500)
         console.log(err)
-
-      });
+      })
   }
 }
 
 const isEmailCompliance = (req, res, next) => {
+  const email = req.body.email
 
-  if (req.body.email === undefined) {
-
+  if (email === undefined || isEmpty(req.body.email)) {
     res.status(404).json({message: 'Email undefined'})
-
   } else {
-
-    const email = req.body.email
-
-    if (isEmail(email)) {
-
-      next()
-
-    } else {
-
-      res.status(422).json({message: 'Email validation error'})
-
-    }
+    if (isEmail(email)) next()
+    else res.status(404).json({message: 'Email validation error'})
   }
 }
 
@@ -114,7 +66,7 @@ const isPasswordCompliance = (req, res, next) => {
   const uppercaseCheckRegex = new RegExp("^(?=.*[A-Z])")
   const specialCheckRegex = new RegExp("^(?=.*[!@#%&])")
 
-  if (req.body.password === undefined) {
+  if (req.body.password === undefined || isEmpty(req.body.password)) {
 
     res.status(404).json({message: 'Password undefined'})
 
@@ -124,23 +76,23 @@ const isPasswordCompliance = (req, res, next) => {
 
     if (!minCharactersRegex.test(password)) {
 
-      res.status(422).json({message: 'Minimum number of characters error'})
+      res.status(404).json({message: 'Minimum number of characters error'})
 
     } else if (!numberCheckRegex.test(password)) {
 
-      res.status(422).json({message: 'Not found at least one digit'})
+      res.status(404).json({message: 'Not found at least one digit'})
 
     } else if (!lowercaseCheckRegex.test(password)) {
 
-      res.status(422).json({message: 'Not found at least one lowercase letter symbol'})
+      res.status(404).json({message: 'Not found at least one lowercase letter symbol'})
 
     } else if (!uppercaseCheckRegex.test(password)) {
 
-      res.status(422).json({message: 'Not found at least one uppercase letter symbol'})
+      res.status(404).json({message: 'Not found at least one uppercase letter symbol'})
 
     } else if (!specialCheckRegex.test(password)) {
 
-      res.status(422).json({message: 'Not found at least one special symbol'})
+      res.status(404).json({message: 'Not found at least one special symbol'})
 
     } else {
 
