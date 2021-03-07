fa76faf1492bdc226ebac9a43615f35d8fafc81f
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2021-03-07 19:15:46 +0200

    modified

--- api/routes.js
+++ api/routes.js
@@ -3,14 +3,15 @@ const routes = require('express').Router()
 const Monuments = require('./routes/Monuments')
 const Users = require('./routes/Users')
 
-const UserService = require('./helpers/UserService')
-const MonumentService = require('./helpers/MonumentService')
+const UserService = require('./services/UserService')
+const MonumentService = require('./services/MonumentService')
 
 const AuthorizationController = require('./Controllers/AuthorizationController')
 const AccessController = require('./Controllers/AccessController')
 const UserController = require('./Controllers/UserController')
 
 const cloudinary = require('./utils/cloudinary')
+const validator = require('./helpers/validator')
 
 if (process.env.NODE_ENV !== 'development') {
 
@@ -44,28 +45,31 @@ if (process.env.NODE_ENV !== 'development') {
 
   //==============================Monuments==============================//
 
-  routes.get('/monuments', Monuments.getMonuments)
-  routes.get('/monuments/:id', Monuments.getMonument)
+  routes.get('/api/monuments', Monuments.getMonuments)
+  routes.get('/api/monuments/:id', Monuments.getMonument)
 
-  routes.post('/monuments', MonumentService.isMonumentExist, MonumentService.validateData, cloudinary.uploadImage, Monuments.createMonument)
+  routes.post('/api/monuments', AccessController.verifyUser, AuthorizationController.MonumentsDB, MonumentService.isMonumentExist, MonumentService.validateData, cloudinary.uploadImage, Monuments.createMonument)
 
-  routes.put('/monuments/:id', Monuments.updateMonument)
+  routes.put('/api/monuments/:id', AccessController.verifyUser, AuthorizationController.MonumentsDB, Monuments.updateMonument)
 
-  routes.delete('/monuments/:id', Monuments.deleteMonument)
-  routes.delete('/monuments/db/all', Monuments.clearMonumentsDB)
+  routes.delete('/api/monuments/:id', AccessController.verifyUser, AuthorizationController.MonumentsDB, Monuments.deleteMonument)
+  // routes.delete('/api/monuments/db/all', Monuments.clearMonumentsDB)
 
   //================================Users================================//
 
-  routes.get('/users', Users.getUsers)
-  routes.get('/users/:email', Users.getUser)
+  routes.get('/api/users', AccessController.verifyUser, AuthorizationController.UserDB, Users.getUsers)
+  routes.get('/api/users/:email', AccessController.verifyUser, AuthorizationController.UserDB, Users.getUser)
+
+  routes.get('/role', UserService.getRole)
 
   routes.post('/login', UserService.isUserDataExist, UserController.SignIn)
-  routes.post('/signup', UserService.isUserExist, UserService.isEmailCompliance, UserService.isPasswordCompliance, UserController.SignUp)
+  routes.post('/signup', UserService.isUserExist, validator.isEmailCompliance, validator.isPasswordCompliance, UserController.SignUp)
+  routes.post('/api/users', AccessController.verifyUser, UserService.isUserExist, AuthorizationController.UserDB, Users.createUser)
 
-  routes.put('/users/:email', Users.updateUser)
+  routes.put('/api/users/:email', AccessController.verifyUser, AuthorizationController.UserDB, Users.updateUser)
 
-  routes.delete('/users/:email', Users.deleteUser)
-  routes.delete('/users/db/all', Users.clearUserDB)
+  routes.delete('/api/users/:email', AccessController.verifyUser, AuthorizationController.UserDB, Users.deleteUser)
+  // routes.delete('/api/users/db/all', Users.clearUserDB)
 
 }
 
