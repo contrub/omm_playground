187f29498e3d77f6ea41d64bd7596d59a2200e76
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2021-03-25 23:02:36 +0200

    forbidden page

---
+++ client/src/pages/forbidden.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {withRouter} from "react-router";
+
+class ForbiddenPage extends React.Component {
+  render() {
+    const pathname = this.props.location.state.from.pathname
+
+    return (
+      <section id="wrapper" className="container-fluid">
+        <div className="error-box">
+          <div className="error-body text-center">
+            <h1 className="text-danger">403</h1>
+            <h3>Forbidden {pathname}</h3>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
+
+
+export default withRouter(ForbiddenPage)
