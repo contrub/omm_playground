154cfbe1265ddcf7cb9b9f102ea89818398afacd
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2021-04-01 14:32:55 +0300

    delete_btn style + image borderRadius

--- client/src/styles/js/monuments_sheet.js
+++ client/src/styles/js/monuments_sheet.js
@@ -1,7 +1,19 @@
 const styles = theme => ({
   image: {
-    maxWidth: '200px',
-    maxHeight: '300px'
+    borderRadius: '10px',
+    maxHeight: '300px',
+    maxWidth: '200px'
+  },
+  edit_btn: {
+    margin: theme.spacing(1, 'auto'),
+    display: 'block',
+    maxWidth: '50px',
+    width: '50px'
+  },
+  delete_btn: {
+    margin: theme.spacing(1, 'auto'),
+    maxWidth: '80px',
+    width: '80px'
   }
 })
 
