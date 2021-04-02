1fd8f8d677d38cdb1caded21e6da901a99d5ef0e
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2021-04-02 22:08:39 +0300

    <Loading/> integration

--- client/src/pages/monuments/monuments_sheet.js
+++ client/src/pages/monuments/monuments_sheet.js
@@ -5,15 +5,22 @@ import {Link} from "react-router-dom";
 
 // Custom components
 import ModalWindow from "../../components/modal";
+import Loading from "../loading";
 
 // Material-UI components
 import withStyles from "@material-ui/core/styles/withStyles";
+import Button from "@material-ui/core/Button";
 
 // Material-UI icons
+import DeleteIcon from "@material-ui/icons/Delete";
 import AddBoxIcon from "@material-ui/icons/AddBox";
+import EditIcon from "@material-ui/icons/Edit";
 
 // Local modules
 import MonumentService from "../../services/MonumentService";
+
+// Third party modules
+import Cookies from "js-cookie";
 
 // Custom styles
 import styles from "../../styles/js/monuments_sheet";
@@ -21,7 +28,9 @@ import styles from "../../styles/js/monuments_sheet";
 class MonumentsSheet extends React.Component {
     state = {
       monuments: [],
+      isLoading: false,
       modal: {
+        redirectURL: '',
         head: '',
         body: ''
       }
@@ -30,6 +39,8 @@ class MonumentsSheet extends React.Component {
     componentDidMount = () => {
       let {modal} = this.state
 
+      this.setState({isLoading: true})
+
       MonumentService.fetchMonuments()
         .then((res) => {
           const monumentsCount = res.length
@@ -37,21 +48,61 @@ class MonumentsSheet extends React.Component {
           if (monumentsCount === 0) {
             modal["head"] = 'Monuments database is empty'
             modal["body"] = 'Please, create new monuments'
+            modal["redirectURL"] = '/create_monument'
             this.setState({modal: modal})
           } else {
             this.setState({monuments: res})
           }
+
+          this.setState({isLoading: false})
         })
         .catch((err) => {
           modal["head"] = 'Server error'
-          modal["body"] = err.message()
-          this.setState({modal: modal})
+          modal["body"] = err.message
+          this.setState({modal: modal, isLoading: false})
         })
     }
 
-    render() {
-      const {classes} = this.props
+    removeMonument = (e, id) => {
+      // const id = e.target.value
       const {monuments} = this.state
+      let {modal} = this.state
+
+      console.log(id)
+
+      const monument = monuments.find(monument => monument._id === id)
+      const imagePublicID = monument.imageURL.split('/')[7] + '/' + monument.imageURL.split('/')[8].split('.')[0]
+
+      MonumentService.deleteMonument({token: Cookies.get('accessToken'), id: id, imagePublicID: imagePublicID})
+        .then((res) => {
+          console.log(res)
+          // локальное удаление
+          MonumentService.fetchMonuments()
+            .then((res) => {
+              if (!res.length) {
+                modal["head"] = 'Monuments database is empty'
+                modal["body"] = 'Please, create new monuments'
+                modal["redirectURL"] = '/create_monument'
+                this.setState({modal: modal})
+              }
+
+              this.setState({monuments: res})
+            })
+        })
+      // отловить ошибку
+    }
+
+    render() {
+      const {monuments} = this.state
+      const {isLoading} = this.state
+      const {classes} = this.props
+
+
+      if (isLoading) {
+        return (
+          <Loading/>
+        )
+      }
 
       return (
         <div>
@@ -104,18 +155,32 @@ class MonumentsSheet extends React.Component {
                     </div>
                   </td>
                   <td>
-                    <Link to={`/edit_monument/${monument._id}`}>
-                      <button className="btn btn-secondary">
-                        <i className="fa fa-edit fa-lg" ></i>
-                      </button>
-                    </Link>
+                    <div>
+                      <Button
+                        href={`/edit_monument/${monument._id}`}
+                        className={classes.edit_btn}
+                        variant="contained"
+                        color="primary"
+                      >
+                        <EditIcon/>
+                      </Button>
+                      <Button
+                        onClick={(e) => this.removeMonument(e, monument._id)}
+                        className={classes.edit_btn}
+                        value={monument._id}
+                        variant="contained"
+                        color="secondary"
+                      >
+                        <DeleteIcon/>
+                      </Button>
+                    </div>
                   </td>
                 </tr>
               )
             })}
             </tbody>
           </table>
-          {this.state.modal.body && <ModalWindow head={this.state.modal.head} body={this.state.modal.body}/>}
+          {this.state.modal.body && <ModalWindow head={this.state.modal.head} body={this.state.modal.body} redirectURL={this.state.modal.redirectURL}/>}
         </div>
       )
     }
--- client/src/pages/users/users_sheet.js
+++ client/src/pages/users/users_sheet.js
@@ -4,6 +4,7 @@ import {Link} from "react-router-dom";
 
 // Custom components
 import ModalWindow from "../../components/modal";
+import Loading from "../loading";
 
 // Material-UI icons
 import AddBoxIcon from '@material-ui/icons/AddBox';
@@ -20,6 +21,7 @@ import "../../styles/css/users_sheet.css";
 class UsersSheet extends React.Component {
   state = {
     users: [],
+    isLoading: false,
     modal: {
       head: '',
       body: ''
@@ -29,53 +31,40 @@ class UsersSheet extends React.Component {
   componentDidMount = () => {
     let {modal} = this.state
 
+    this.setState({isLoading: true})
+
     UserService.getUsers({token: Cookies.get('accessToken')})
       .then((res) => {
         const usersCount = res.length
-
+        // Если не будет пользователей вовсе, то как человек получил доступ к данной странице ?
         if (usersCount === 0) {
           modal["head"] = 'Users database is empty'
           modal["body"] = 'Please, create new users'
+          modal["redirectURL"] = '/create_user'
           this.setState({modal: modal})
         } else {
           this.setState({users: res})
         }
+
+        this.setState({isLoading: false})
       })
       .catch((err) => {
         modal["head"] = 'Server error'
         modal["body"] = err.message
+        modal["redirectURL"] = '/'
         this.setState({modal: modal})
       })
   }
 
   render() {
-    const users = this.state.users.map((user, index) => {
+    const {isLoading} = this.state
+    const {users} = this.state
+
+    if (isLoading) {
       return (
-        <tr key={index}>
-          <td>{user.email}</td>
-          <td>{user.userRole}</td>
-          <td>{user._id}</td>
-          <td>
-            {user.status === 'active' ?
-              <button className="btn btn-success" disabled>
-                Active
-              </button>
-              :
-              <button className="btn btn-danger" disabled>
-              Disable
-              </button>
-            }
-          </td>
-          <td>
-            <Link to={`/users/${user.email}`}>
-              <button className="btn btn-secondary">
-                <i className="fa fa-edit fa-lg" ></i>
-              </button>
-            </Link>
-          </td>
-        </tr>
+        <Loading/>
       )
-    })
+    }
 
     return (
       <div>
@@ -94,7 +83,33 @@ class UsersSheet extends React.Component {
             </tr>
             </thead>
             <tbody>
-              {users}
+              {users.map((user, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{user.email}</td>
+                    <td>{user.userRole}</td>
+                    <td>{user._id}</td>
+                    <td>
+                      {user.status === 'active' ?
+                        <button className="btn btn-success" disabled>
+                          Active
+                        </button>
+                        :
+                        <button className="btn btn-danger" disabled>
+                          Disable
+                        </button>
+                      }
+                    </td>
+                    <td>
+                      <Link to={`/users/${user.email}`}>
+                        <button className="btn btn-secondary">
+                          <i className="fa fa-edit fa-lg" ></i>
+                        </button>
+                      </Link>
+                    </td>
+                  </tr>
+                )
+              })}
             </tbody>
         </table>
         {this.state.modal.body && <ModalWindow head={this.state.modal.head} body={this.state.modal.body}/>}
