ddc1a07268bacd0fbf7f3d5202c65895313367af
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2021-04-06 17:06:10 +0300

    verifyAction integrated

--- api/src/Controllers/AuthController.js
+++ api/src/Controllers/AuthController.js
@@ -4,52 +4,75 @@ const User = require('../models/User')
 // Local class
 const ApiError = require('../error/ApiError')
 
-const UserDB = (req, res, next) => {
-  const email = req.decoded.email
+// Local resources
+const permissions = require('../resources/permissions')
+
+// const UserDB = (req, res, next) => {
+//   const email = req.decoded.email
+//
+//   User
+//     .find({email: email})
+//     .then((user) => {
+//       if (!user.length) {
+//         next(ApiError.custom(401, 'Email undefined in DB'))
+//       } else {
+//         const userRole = user[0].userRole
+//         const userAccessGroup = process.env.USERS_DB_ACCESS_GROUP.split(',')
+//
+//         userAccessGroup.forEach((entry, index) => {
+//           userAccessGroup[index] = entry.replace(/\W/g, '')
+//         })
+//
+//         if (!userAccessGroup.includes(userRole)) {
+//           next(ApiError.custom(403, 'UsersDB access denied'))
+//         }
+//       }
+//     })
+// }
+
+const verifyAction = (req, action, res, next) => {
+  const email = req.decoded ? req.decoded.email :  ''
 
   User
     .find({email: email})
     .then((user) => {
-      if (!user.length) {
-        next(ApiError.custom(401, 'Email undefined in DB'))
-      } else {
-        const userRole = user[0].userRole
-        const userAccessGroup = process.env.USERS_DB_ACCESS_GROUP.split(',')
+      const userRole = user.length ? user[0].userRole : 'guest'
+      const currentPermissions = Object.values(permissions)[Object.keys(permissions).indexOf(userRole)]
 
-        userAccessGroup.forEach((entry, index) => {
-          userAccessGroup[index] = entry.replace(/\W/g, '')
-        })
+      console.log(action)
+      console.log(currentPermissions)
+      console.log(currentPermissions.includes(action))
 
-        if (!userAccessGroup.includes(userRole)) {
-          next(ApiError.custom(403, 'UsersDB access denied'))
-        }
+      if (!currentPermissions.includes(action)) {
+        next(ApiError.custom(403, 'Access denied'))
       }
     })
 }
 
-const MonumentsDB = async (req, res, next) => {
-  const email = req.decoded.email
-
-  User
-    .find({email: email})
-    .then(user => {
-      if (!user.length) {
-        next(ApiError.custom(401, 'Email undefined in DB'))
-      } else {
-        const userRole = user[0].userRole
-        const userAccessGroup = process.env.MONUMENTS_DB_ACCESS_GROUP.split(',')
-          userAccessGroup.forEach((entry, index) => {
-            userAccessGroup[index] = entry.replace(/\W/g, '')
-          })
-
-        if (!userAccessGroup.includes(userRole)) {
-          next(ApiError.custom(403, 'Monuments access denied'))
-        }
-      }
-    })
-}
+// const MonumentsDB = async (req, res, next) => {
+//   const email = req.decoded.email
+//
+//   User
+//     .find({email: email})
+//     .then(user => {
+//       if (!user.length) {
+//         next(ApiError.custom(401, 'Email undefined in DB'))
+//       } else {
+//         const userRole = user[0].userRole
+//         const userAccessGroup = process.env.MONUMENTS_DB_ACCESS_GROUP.split(',')
+//           userAccessGroup.forEach((entry, index) => {
+//             userAccessGroup[index] = entry.replace(/\W/g, '')
+//           })
+//
+//         if (!userAccessGroup.includes(userRole)) {
+//           next(ApiError.custom(403, 'Monuments access denied'))
+//         }
+//       }
+//     })
+// }
 
 module.exports = {
-  UserDB: UserDB,
-  MonumentsDB: MonumentsDB
+  // UserDB: UserDB,
+  // MonumentsDB: MonumentsDB,
+  verifyAction: verifyAction
 }
