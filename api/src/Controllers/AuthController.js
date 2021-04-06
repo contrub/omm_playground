11f5b1bec9a5837eeebc43ce3ad1949a8b3b94f1
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2021-04-06 19:00:25 +0300

    remove unused functions

--- api/src/Controllers/AuthController.js
+++ api/src/Controllers/AuthController.js
@@ -7,29 +7,6 @@ const ApiError = require('../error/ApiError')
 // Local resources
 const permissions = require('../resources/permissions')
 
-// const UserDB = (req, res, next) => {
-//   const email = req.decoded.email
-//
-//   User
-//     .find({email: email})
-//     .then((user) => {
-//       if (!user.length) {
-//         next(ApiError.custom(401, 'Email undefined in DB'))
-//       } else {
-//         const userRole = user[0].userRole
-//         const userAccessGroup = process.env.USERS_DB_ACCESS_GROUP.split(',')
-//
-//         userAccessGroup.forEach((entry, index) => {
-//           userAccessGroup[index] = entry.replace(/\W/g, '')
-//         })
-//
-//         if (!userAccessGroup.includes(userRole)) {
-//           next(ApiError.custom(403, 'UsersDB access denied'))
-//         }
-//       }
-//     })
-// }
-
 const verifyAction = (req, action, res, next) => {
   const email = req.decoded ? req.decoded.email :  ''
 
@@ -49,30 +26,6 @@ const verifyAction = (req, action, res, next) => {
     })
 }
 
-// const MonumentsDB = async (req, res, next) => {
-//   const email = req.decoded.email
-//
-//   User
-//     .find({email: email})
-//     .then(user => {
-//       if (!user.length) {
-//         next(ApiError.custom(401, 'Email undefined in DB'))
-//       } else {
-//         const userRole = user[0].userRole
-//         const userAccessGroup = process.env.MONUMENTS_DB_ACCESS_GROUP.split(',')
-//           userAccessGroup.forEach((entry, index) => {
-//             userAccessGroup[index] = entry.replace(/\W/g, '')
-//           })
-//
-//         if (!userAccessGroup.includes(userRole)) {
-//           next(ApiError.custom(403, 'Monuments access denied'))
-//         }
-//       }
-//     })
-// }
-
 module.exports = {
-  // UserDB: UserDB,
-  // MonumentsDB: MonumentsDB,
   verifyAction: verifyAction
 }
