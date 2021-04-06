078c6fe29fd0f9db0dfb6b995221518e61fb3830
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2021-04-06 21:55:57 +0300

    bug fixed

--- api/src/error/api-error-handler.js
+++ api/src/error/api-error-handler.js
@@ -4,11 +4,10 @@ const ApiError = require('./ApiError')
 function apiErrorHandler(err, req, res, next) {
 
   if (err instanceof ApiError) {
-    res.status(err.code).json({message: err.message})
-    return;
+    res.status(err.statusCode).json({message: err.message})
+  } else {
+    res.status(500).send()
   }
-
-  res.sendStatus(500)
 }
 
 module.exports = apiErrorHandler
