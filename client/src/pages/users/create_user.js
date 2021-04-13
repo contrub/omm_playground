25a90042b25802b65c15992f9fbd6cd3bacea958
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2021-04-13 13:52:04 +0300

    updated

--- client/src/pages/users/create_user.js
+++ client/src/pages/users/create_user.js
@@ -47,21 +47,26 @@ class CreateUser extends React.Component {
     errors: {
       validation: ''
     },
-    isValid: false
+    isValid: false,
+    isPasswordHidden: true
   }
 
   changeModalState = (state) => {
     let {modal} = this.state
+
     modal["isOpen"] = state
 
     this.setState({modal: modal})
   }
 
-  handleFieldValidation = () => {
-    this.setState({isValid: emailValidation(this.state.inputs, this.state.errors)})
-    this.setState({isValid: passwordValidation(this.state.inputs)})
+  handleFieldValidation = (name) => {
+    let {inputs, errors} = this.state
 
-    document.getElementById('passwordRequirements').hidden = false
+    if (name === 'email') {
+      this.setState({isValid: emailValidation(inputs, errors)})
+    } else if (name === 'password') {
+      this.setState({isValid: passwordValidation(inputs, errors)})
+    }
   }
 
   contactSubmit = (e) => {
@@ -96,7 +101,6 @@ class CreateUser extends React.Component {
     } else {
       errors["validation"] = "ValidationError"
 
-      this.handleFieldValidation()
       this.setState({errors: errors})
     }
   }
@@ -105,17 +109,20 @@ class CreateUser extends React.Component {
     let inputs = this.state.inputs
 
     inputs[input] = e.target.value
+
     this.setState({input: inputs[input]})
-    this.handleFieldValidation()
+    this.handleFieldValidation(input)
   }
 
   showPassword = () => {
-    document.getElementById('password').type === 'password' ? document.getElementById('password').type = 'text' : document.getElementById('password').type = 'password'
+    let {isPasswordHidden} = this.state
+
+    this.setState({isPasswordHidden: !isPasswordHidden})
   }
 
   render() {
     const {classes} = this.props
-    const {inputs, errors, modal} = this.state
+    const {inputs, errors, modal, isPasswordHidden} = this.state
 
     return (
       <div>
@@ -141,7 +148,7 @@ class CreateUser extends React.Component {
                 autoComplete="email"
                 autoFocus
               />
-              <div className={classes.errors}>{errors["email"]}</div>
+              <div className={classes.emailRequirement}>{errors["email"]}</div>
               <TextField
                 onChange={this.handleChange.bind(this, "password")}
                 value={inputs["password"]}
@@ -159,29 +166,27 @@ class CreateUser extends React.Component {
                 fullWidth
                 name="password"
                 label="Password"
-                type="password"
+                type={isPasswordHidden ? 'password' : 'text'}
                 id="password"
                 autoComplete="current-password"
               />
-              <ul id='passwordRequirements' hidden>
-                <li className={classes.pass_requirement} id='quantityCheck'>At least 8 characters</li>
-                <li className={classes.pass_requirement} id='numberCheck'>Contains at least 1 number</li>
-                <li className={classes.pass_requirement} id='lowercaseCheck'>Contains at least lowercase letter</li>
-                <li className={classes.pass_requirement} id='uppercaseCheck'>Contains at least uppercase letter</li>
-                <li className={classes.pass_requirement} id='specialCharacterCheck'>Contains a special character (!@#%&)</li>
+              <ul className={classes.passwordRequirements}>
+                {!errors["quantityCheck"] && <li>At least 8 characters</li>}
+                {!errors["numberCheck"] && <li>Contains at least 1 number</li>}
+                {!errors["lowercaseCheck"] && <li>Contains at least lowercase letter</li>}
+                {!errors["uppercaseCheck"] && <li>Contains at least uppercase letter</li>}
+                {!errors["specialCharacterCheck"] && <li>Contains a special character (!@#%&)</li>}
               </ul>
               <Button
                 type="submit"
                 fullWidth
                 variant="contained"
                 color="primary"
-                className={classes.submit_btn}
+                className={classes.submitBtn}
               >
                 Create
               </Button>
-              <div className={classes.errors}>
-                {errors.validation}
-              </div>
+              <div className={classes.validError}>{errors.validation}</div>
             </form>
           </div>
         </Container>
