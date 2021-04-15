b4a437286be479908eed672ce0e827008852542a
EgorOnufreychuk <onufreychuk.egor@rl.odessa.ua>
2021-04-15 19:43:20 +0300

    updated

--- api/src/utils/cloudinary.js
+++ api/src/utils/cloudinary.js
@@ -16,7 +16,7 @@ cloudinary.config({
 uploadImage = async (req, res) => {
   const fileStr = req.body.base64
 
-  if (fileStr !== undefined && !isEmpty(fileStr)) {
+  if (fileStr) {
     await cloudinary.uploader.upload(fileStr, {folder: process.env.CLOUDINATY_FOLDER})
       .then((res) => {
         const url = res.url
@@ -32,7 +32,7 @@ uploadImage = async (req, res) => {
 deleteImage = async (req, res) => {
   const imagePublicID = req.body.imagePublicID
 
-  if (imagePublicID !== undefined && !isEmpty(imagePublicID) && imagePublicID !== process.env.DEFAULT_MONUMENT_IMAGE_PUBLIC_ID) {
+  if (imagePublicID && imagePublicID !== process.env.DEFAULT_MONUMENT_IMAGE_PUBLIC_ID) {
     await cloudinary.api.delete_resources([imagePublicID])
       .catch((err) => {
         // console.log(err)
